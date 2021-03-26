1642eb8226a0c8edbd5a892c56a4b04d32eb0dc0
jjamel2512 <62228543+jjamel2512@users.noreply.github.com>
2021-03-26 17:22:13 -0400

    Create finsihedBinarycodeformtemplate.py

---
+++ finsihedBinarycodeformtemplate.py
@@ -0,0 +1,20 @@
+'''
+
+#Hello My name is binary_code_form;
+
+#Hello My Name Is binary_form_code:
+
+#{2}
+#[3]
+#{Li]
+#[Si} | |
+"""
+'''
+#$help_print[{2[si}]
+#$ig_bliss_exit
+#$Mother_of_codes
+#$new_gen_22nd_21st_cent
+#$help_print[{$new_gen_22nd_21st_cent}] | |
+#$professor_ig_bliss_exit
+#$help_data_chess_exec_form_binary_code_form
+#$exec_help_print[$help_data_chess_exec_form_binary_code_form}] ||
