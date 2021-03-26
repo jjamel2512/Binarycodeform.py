972ab67e034e306d6815ee87a258514b22661da5
jjamel2512 <62228543+jjamel2512@users.noreply.github.com>
2021-03-26 17:12:18 -0400

    Create newgenfile.py

---
+++ newgenfile.py
@@ -0,0 +1,17 @@
+'''
+
+#Hello My name is binary_code_form;
+
+#Hello My Name Is binary_form_code:
+
+#{2}
+#[3]
+#{Li]
+#[Si} | |
+"""
+'''
+#$help_print[{2[si}]
+#$ig_bliss_exit
+#$Mother_of_codes
+#$new_gen_22nd_21st_cent
+#$help_print[{$new_gen_22nd_21st_cent}] | |
